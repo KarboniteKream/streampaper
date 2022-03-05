2216152b4279eeabcf76cf2d20458da092b3791f
Klemen Košir <klemen.kosir@kream.io>
2022-03-05 14:34:31 +0900

    Update source URLs

--- migrations/2021-10-02-120000_create_sources/up.sql
+++ migrations/2021-10-02-120000_create_sources/up.sql
@@ -16,4 +16,4 @@ CREATE INDEX idx_sources_updated_at
 INSERT INTO sources (id, name, typ, url, playlist, enabled) VALUES
     (1, 'Kamikochi', 2, 'https://www.youtube.com/watch?v=9-sfWSHtJdk', null, 1),
     (2, 'KamniskoSedlo', 1, 'http://pdkamnik.si/watermark.php?filename=sedlo.jpg', null, 1),
-    (3, 'PlansarskoJezero', 3, null, 'https://stream.pikacom.com/Jezersko/Jezersko.stream/playlist.m3u8', 1);
+    (3, 'PlansarskoJezero', 3, null, 'https://livestream.alpejadran.si/Jezersko/Jezersko.stream/playlist.m3u8', 1);
